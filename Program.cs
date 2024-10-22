6a58a6963a55435ab9b2341d07583d95890f9e15
CarLos Daniel <carlos807.cf@gmail.com>
2024-10-22 08:18:51 -0300

    DESAFIO FINALIZADOgit add .!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone i = new Iphone("123684255","iphone 11","46557557",16);
+i.Ligar();
+i.InstalarAplicativo("Telegram");
+
+Console.WriteLine("./");
+
+Nokia n =  new Nokia("654754","GH3","5646856",8);
+n.ReceberLigacao();
+n.InstalarAplicativo("Whatshap");
